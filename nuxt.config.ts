c1610b35f712f136d06d3ef8eecba97971aa2a03
moha <25137956+FunnyPhantom@users.noreply.github.com>
2024-07-26 22:54:08 +0200

    chore: Add @pinia/nuxt and @nuxtjs/google-fonts dependencies
    This commit adds the @pinia/nuxt and @nuxtjs/google-fonts dependencies to the project. These modules are required for the application to work properly.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt", //
+    "@nuxtjs/google-fonts",
+  ],
+  css: ["normalize.css/normalize.css"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        sass: {
+          additionalData: `@use "~/assets/variables.sass" as *\n`,
+        },
+      },
+    },
+  },
+  googleFonts: {
+    preload: true,
+    families: {
+      "Cormorant Infant": true,
+      "Space Grotesk": true,
+    },
+  },
+});
